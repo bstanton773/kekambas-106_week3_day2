b55fc4de12c90f60a50e7b2d099b774028f6593a
Brian <bstanton773@gmail.com>
2022-12-13 15:20:45 -0600

    Afternoon lecture + modules

---
+++ folder_module/__init__.py
@@ -0,0 +1,5 @@
+print('The __init__.py is being run')
+
+
+def add_nums(num1, num2):
+    return num1 + num2
---
+++ test_module.py
@@ -0,0 +1,14 @@
+print('Hello this is the test module!')
+
+
+def greet(name):
+    print(f"Hello {name.title()}, how are you today? I hope you are doing well!!")
+
+def leave(name):
+    print(f"Goodbye {name.title()}. It was a pleasure spending time with you")
+
+
+# Test to make sure functions work correctly
+# greet('brian')
+# leave('brian')
+
